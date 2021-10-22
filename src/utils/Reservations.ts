8090cadac37df526db1a779f9ed9371ca94f56c9
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-22 11:27:53 -0400

    adding fake reservations data

---
+++ src/utils/Reservations.ts
@@ -0,0 +1,124 @@
+interface Reservation {
+
+    date: string;
+    time: string;
+    ammountOfPersons: number;
+    isReserved: boolean;
+}
+
+export const Reservations: Reservation[] = [
+    {
+        date: "22/10/2021",
+        time: "10:30",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "10:45",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "11:00",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "11:15",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "11:30",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "11:45",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "12:00",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "12:14",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "12:30",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "12:45",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "13:00",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "13:15",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "13:30",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "13:45",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "14:00",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "14:15",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "14:30",
+        ammountOfPersons: 9,
+        isReserved: false
+    },
+    {
+        date: "22/10/2021",
+        time: "14:45",
+        ammountOfPersons: 9,
+        isReserved: true
+    },
+    {
+        date: "22/10/2021",
+        time: "15:00",
+        ammountOfPersons: 9,
+        isReserved: false
+    }
+]
