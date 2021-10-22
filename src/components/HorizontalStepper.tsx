3c356f75dd155ff5319d16797487a54e8aa1b9d4
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-22 10:38:36 -0400

    horizontal stepper style refactor

--- src/components/HorizontalStepper.tsx
+++ src/components/HorizontalStepper.tsx
@@ -1,32 +1,29 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Typography from '@mui/material/Typography';
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
 
-const steps = ['Busca una mesa', 'Agrega tu información'];
+const steps = ['Busca una mesa', 'Agrega tu información']
 
-export default function HorizontalStepperWithError() {
+const HorizontalStepper = () => (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
+        <Box sx={{ width: '60.5%' }}>
+            <Stepper activeStep={1}>
+                {steps.map(label => {
+                    const labelProps: {
+                        optional?: React.ReactNode
+                        error?: boolean
+                    } = {}
+                    return (
+                        <Step key={label}>
+                            <StepLabel {...labelProps}>{label}</StepLabel>
+                        </Step>
+                    )
+                })}
+            </Stepper>
+        </Box>
+    </div>
+)
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-            <Box sx={{ width: '60%' }}>
-                <Stepper activeStep={1}>
-                    {steps.map((label, index) => {
-                        const labelProps: {
-                            optional?: React.ReactNode;
-                            error?: boolean;
-                        } = {};
-
-                        return (
-                            <Step key={label}>
-                                <StepLabel {...labelProps}>{label}</StepLabel>
-                            </Step>
-                        );
-                    })}
-                </Stepper>
-            </Box>
-        </div>
-    );
-}
+export default HorizontalStepper
