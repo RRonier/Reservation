58e303f33a91df85fce07ec428d55c570285c83c
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-22 11:29:41 -0400

    adding reservation component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Reservation from "./views/Reservation"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Reservation />
+  </>
+)
 
-export default App;
+export default App
