7566909cd9a510d876c50e3c22fe861d31c30af8
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-22 12:59:48 -0400

    Horizontal stepper handles what page to display

--- src/components/HorizontalStepper.tsx
+++ src/components/HorizontalStepper.tsx
@@ -1,29 +1,33 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import Stepper from '@mui/material/Stepper'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
 
 const steps = ['Busca una mesa', 'Agrega tu información']
-
-const HorizontalStepper = () => (
-    <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-        <Box sx={{ width: '60.5%' }}>
-            <Stepper activeStep={1}>
-                {steps.map(label => {
-                    const labelProps: {
-                        optional?: React.ReactNode
-                        error?: boolean
-                    } = {}
-                    return (
-                        <Step key={label}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
-                        </Step>
-                    )
-                })}
-            </Stepper>
-        </Box>
-    </div>
-)
+interface IProps {
+    activeStep: number
+}
+const HorizontalStepper = ({ activeStep }: IProps) => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
+            <Box sx={{ width: '60.5%' }}>
+                <Stepper activeStep={activeStep}>
+                    {steps.map(label => {
+                        const labelProps: {
+                            optional?: React.ReactNode
+                            error?: boolean
+                        } = {}
+                        return (
+                            <Step key={label}>
+                                <StepLabel {...labelProps}>{label}</StepLabel>
+                            </Step>
+                        )
+                    })}
+                </Stepper>
+            </Box>
+        </div>
+    )
+}
 
 export default HorizontalStepper
