ef4921f54fd415e6511651d8589b50b1554e2cf6
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:40:29 -0400

    adding the checkbox as custom component

---
+++ src/components/CustomCheckbox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FormControlLabel, Checkbox } from '@mui/material'
+
+interface IProps {
+    label: string;
+}
+
+const CustomCheckbox = ({ label }: IProps) => {
+    return (
+        <FormControlLabel control={<Checkbox />} label={label} />
+    )
+}
+export default CustomCheckbox
