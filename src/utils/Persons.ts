88d3768b3499f1ca4b7b4658073b6d2ce281c505
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:45:38 -0400

    adding person interface type and array data

---
+++ src/utils/Persons.ts
@@ -0,0 +1,24 @@
+interface Person {
+    ammount: number;
+}
+
+export const persons: readonly Person[] = [
+    {
+        ammount: 1
+    },
+    {
+        ammount: 2
+    },
+    {
+        ammount: 4
+    },
+    {
+        ammount: 6
+    },
+    {
+        ammount: 8
+    },
+    {
+        ammount: 10
+    },
+];
