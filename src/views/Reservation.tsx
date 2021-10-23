42cf11941a18cc756767b259f510a032888a4c69
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:40:11 -0400

    refactoring reservation page

--- src/views/Reservation.tsx
+++ src/views/Reservation.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import { Button, Divider } from '@mui/material'
-import HorizontalStepper from 'components/HorizontalStepper'
-import FirstStepPage from './FirstStepPage'
+import { HorizontalStepper } from 'components'
+import {
+    FirstStepPage,
+    SecondStepPage
+} from 'views'
 
 const Reservation = () => {
     const [activeStep, setActiveStep] = useState<number>(0)
     const [tableSelected, setTableSelected] = useState<boolean>(false)
 
-    const handleNext = () => {
+    const handleNext = (): void => {
         setActiveStep(activeStep + 1)
     }
 
@@ -16,14 +19,20 @@ const Reservation = () => {
     }
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
             <div>
                 <HorizontalStepper activeStep={activeStep} />
                 <Divider variant="inset" />
-                {activeStep === 0 ? <FirstStepPage selectTable={selectTable} /> : null}
-                <div style={{ display: 'grid', marginTop: '10px' }}>
-                    <Button variant="contained" size="large" onClick={handleNext} disabled={!tableSelected}>Continuar</Button>
-                </div>
+                {activeStep === 0 ? (
+                    <>
+                        <FirstStepPage selectTable={selectTable} />
+                        <div style={{ display: 'grid', marginTop: '10px' }}>
+                            <Button variant="contained" onClick={handleNext} disabled={!tableSelected}>Continuar</Button>
+                        </div>
+                    </>
+                ) : (
+                    <SecondStepPage handleNext={handleNext} />
+                )}
             </div>
         </div >
     )
