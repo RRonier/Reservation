b36d7762fb40e86416a92ce553aadbed4f6b7ce3
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:38:46 -0400

    creating custom export for pages

---
+++ src/views/index.ts
@@ -0,0 +1,9 @@
+import FirstStepPage from "./FirstStepPage";
+import SecondStepPage from "./SecondStepPage";
+import Reservation from "./Reservation";
+
+export {
+    FirstStepPage,
+    SecondStepPage,
+    Reservation
+}
