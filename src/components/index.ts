6e6e9908352eebb788bc6ac9c4cbfe55f0874414
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:39:00 -0400

    creating custom export for components

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import CustomCheckbox from "./CustomCheckbox"
+import CustomDatePicker from "./CustomDatePicker"
+import CustomSelect from "./CustomSelect"
+import CustomTimePicker from "./CustomTimePicker"
+import Restorant from "./Restorant"
+import ReservationForm from "./ReservationForm"
+import HorizontalStepper from "./HorizontalStepper"
+
+export {
+    CustomCheckbox,
+    CustomDatePicker,
+    CustomSelect,
+    CustomTimePicker,
+    Restorant,
+    ReservationForm,
+    HorizontalStepper
+}
