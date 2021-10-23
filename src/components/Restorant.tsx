9eb05e23fc400ffa2e4899f65e0b3b9ae45858de
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-10-23 17:45:05 -0400

    code refactor to restorant component

--- src/components/Restorant.tsx
+++ src/components/Restorant.tsx
@@ -1,24 +1,41 @@
-import * as React from 'react'
+import React from 'react'
 import Rating from '@mui/material/Rating'
 import HotelPicture from "assets/small.jpg"
+import DinnerDiningOutlinedIcon from '@mui/icons-material/DinnerDiningOutlined'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
 
 interface IProps {
     name: string,
-    rating: number,
-    chain: string
+    rating?: number,
+    chain?: string
 }
 
-const Hotel = ({ name, rating, chain }: IProps) => (
+const Restorant = ({ name, rating, chain }: IProps) => (
     <div style={{ display: 'flex', height: '70px' }}>
         <img src={HotelPicture} alt="Americana Hotel Preview" style={{ borderRadius: '10%' }} />
         <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '15px' }}>
             {name}
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Rating value={rating} /><p style={{ padding: '0', margin: '0' }}>251 reseñas</p>
-            </div>
+            {rating ?
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Rating value={rating} /><p style={{ padding: '0', margin: '0' }}>251 reseñas</p>
+                </div>
+                :
+                <div style={{ display: 'flex', marginTop: '5px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', margin: '0 20px 0 0' }}>
+                        <DinnerDiningOutlinedIcon style={{ marginRight: '5px' }} />Dom, 31 de oct.
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', margin: '0 20px 0 0' }}>
+                        <AccessTimeIcon style={{ marginRight: '5px' }} /> 10:00
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', margin: '0 20px 0 0' }}>
+                        <PersonOutlineOutlinedIcon style={{ marginRight: '5px' }} /> 2 persons
+                    </div>
+                </div>
+            }
             {chain}
         </div>
     </div>
 )
 
-export default Hotel
+export default Restorant
